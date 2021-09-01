c18a497c2e70dfb7fce9f27ada3daf2ebec6e88c
Claken <samychouam@gmail.com>
2021-09-01 19:53:32 +0200

    pipex 01 09 21

--- pipex.c
+++ pipex.c
@@ -12,18 +12,20 @@
 
 #include "pipex.h"
 
-void
+int
 	ft_close_fdz(int *fd1, int *fd2)
 {
 	close(*fd1);
 	close(*fd2);
+	return (0);
 }
 int
 	main(int ac, char **av)
 {
-	int	fd1;
-	int	fd2;
-	int	fd3[2];
+	int		fd1;
+	int		fd2;
+	int		fd3[2];
+	pid_t	procid;
 	// fd pour le pipe :
 	// - fd[0] = pour read dans le pipe
 	// - fd[1] = pour write dans le pipe
@@ -34,19 +36,33 @@ int
 	fd1 = open(av[1], O_RDONLY);
 	fd2 = open(av[4], O_RDONLY);
 	if (pipe(fd3) == -1)
-		return (0);
-	printf("fd1 = %d\nfd2 = %d\n", fd1, fd2);
+		return (ft_close_fdz(&fd1, &fd2));
+	procid = fork();
+	if (procid == -1)
+		return (ft_close_fdz(&fd1, &fd2));
+	if (procid == 0)
+	{
+		printf("child\n");
+		printf("fd1 = %d\nfd2 = %d\n", fd1, fd2);
+	}
+	else
+	{
+		wait(NULL);
+		printf("parent\n");
+	}
+	if (fd1 == -1)
+	{
+
+	}
 	if (fd2 == -1)
 	{
 		fd2 = open(av[4], O_CREAT | O_WRONLY | O_TRUNC, 00644);
 		if (fd2 == -1)
 		{
 			printf("fd2 %d\n", fd2);
-			ft_close_fdz(&fd1, &fd2);
-			return (0);
+			return (ft_close_fdz(&fd1, &fd2));
 		}
 	}
-	printf("nice\n");
-	ft_close_fdz(&fd1, &fd2);
-	return (0);
+	printf("nice\n\n");
+	return (ft_close_fdz(&fd1, &fd2));
 }
