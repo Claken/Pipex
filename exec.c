6ee395d400cfcddeec9a15a8663d5e43ab0600a3
Claken <samychouam@gmail.com>
2021-09-08 20:25:37 +0200

    pipex 08 09 2021 part 2

--- exec.c
+++ exec.c
@@ -1,51 +1,94 @@
 #include "pipex.h"
 
+char
+	**ft_tab_of_path(char **envp)
+{
+	int	i;
+	char**tab;
+
+	i = 0;
+	while (!(ft_strnstr(envp[i], "PATH", 4)))
+		i++;
+	tab = ft_split(envp[i], "=:");
+	if (tab == NULL)
+		return (NULL);
+	return (tab);
+}
+
+void
+	ft_print_and_free_tab(char **tab)
+{
+	int i;
+
+	i = -1;
+	while (tab[++i])
+	{
+		printf("%s\n", tab[i]);
+		free(tab[i]);
+	}
+	free(tab);
+}
+
+char
+	*ft_find_cmd(char **path, char *cmd)
+{
+	int	i;
+	char*pathdebut;
+	char*cmdpath;
+	int	fd;
+
+	i = 0;
+	while (path[++i])
+	{
+		pathdebut = ft_strjoin(path[i], "/");
+		if (!pathdebut)
+			return (NULL);
+		cmdpath = ft_strjoin(pathdebut, cmd);
+		if (!cmdpath)
+			return (NULL);
+		free(pathdebut);
+		fd = open(cmdpath, O_RDONLY);
+		if (fd != -1)
+		{
+			close(fd);
+			return (cmdpath);
+		}
+		close(fd);
+		free(cmdpath);
+	}
+	return (NULL);
+}
+
 int
 	main(int ac, char **av, char **envp)
 {
-	printf("Main program started\n");
 	pid_t procid;
 	pid_t procid2;
 	int   pi[2];
 	int   fd1;
 	int   fd2;
-	char* cmd1[3] = { "/bin/ls", "-l", NULL };
-	char* cmd2[3] = { "/usr/bin/wc", "-l", NULL };
-
 	char **cmdn1;
 	char **cmdn2;
+	char **path;
+	char *cmd1path;
+	char *cmd2path;
 
+	if (ac != 5)
+		return (0);
+	printf("Main program started\n");
 	cmdn1 = ft_split(av[2], " ");
 	cmdn2 = ft_split(av[3], " ");
+	path = ft_tab_of_path(envp);
 
-	int i;
-	int j;
+	cmd1path = ft_find_cmd(path, cmdn1[0]);
+	cmd2path = ft_find_cmd(path, cmdn2[0]);
+	printf("%s\n", cmd1path);
+	printf("%s\n", cmd2path);
 
-	i = -1;
-	while (cmdn1[++i])
-	{
-		printf("%s\n", cmdn1[i]);
-		free(cmdn1[i]);
-	}
-	free(cmdn1);
+	char* cmd1[3] = { cmd1path, cmdn1[1], NULL };
+	char* cmd2[3] = { cmd2path, cmdn2[1], NULL };
 
-	i = -1;
-	while (cmdn2[++i])
-	{
-		printf("%s\n", cmdn2[i]);
-		free(cmdn2[i]);
-	}
-	free(cmdn2);
 
-	i = -1;
-
-	while (envp[++i])
-	{
-		if (ft_strnstr(envp[i], "PATH", 4))
-		{
-			printf("%s\n", envp[i]);
-		}
-	}
 
 	if (pipe(pi) == -1)
 		return (0);
@@ -90,5 +133,10 @@ int
 			while (wait(NULL) != -1);
 		}
 	}
+	ft_print_and_free_tab(cmdn1);
+	ft_print_and_free_tab(cmdn2);
+	ft_print_and_free_tab(path);
+	free(cmd1path);
+	free(cmd2path);
 	return (0);
 }
