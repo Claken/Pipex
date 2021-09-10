189ffe9f569267dd25fe42cefbe48d9bcb77bf60
Claken <samychouam@gmail.com>
2021-09-10 03:20:54 +0200

    pipex 10 09 21 part 2

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: sachouam <sachouam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 17:33:40 by sachouam          #+#    #+#             */
-/*   Updated: 2021/09/10 01:46:47 by sachouam         ###   ########.fr       */
+/*   Updated: 2021/09/10 03:18:32 by sachouam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,65 +27,94 @@ static void
 }
 
 static void
-	ft_close_fdz(int *fd1, int *fd2)
+	ft_set_struc(t_prcs *process)
 {
-	close(*fd1);
-	close(*fd2);
+	process->fd = 0;
+	process->id = 0;
+	process->cmd = NULL;
+}
+/*
+static void
+	ft_first_fork(t_prcs *process1, int **pi, char **envp, char *arg)
+{
+	process1->fd = open(arg, O_RDONLY);
+	close(pi[0]);
+	dup2(pi[1], 1);
+	if (process1->fd != -1)
+	{
+		dup2(process1->fd, 0);
+		execve(process1->cmd[0], process1->cmd, envp);
+	}
+	perror("pipex");
+	exit(0);
 }
 
+static int
+	ft_second_fork(t_prcs *process2, int **pi, char **envp, char *arg)
+{
+	process2->fd = open(arg, O_CREAT | O_WRONLY | O_TRUNC, 00644);
+	close(pi[1]);
+	dup2(pi[0], 0);
+	if (process2->fd != -1)
+	{
+		dup2(process2->fd, 1);
+		execve(process2->cmd[0], process2->cmd, envp);
+	}
+	perror("pipex");
+	exit(0);
+}
+*/
 int
 	main(int ac, char **av, char **envp)
 {
-	pid_t	procid;
-	pid_t	procid2;
 	int		pi[2];
-	int		fd1;
-	int		fd2;
-	char	**cmd1;
-	char	**cmd2;
 	char	**path;
+	t_prcs	process1;
+	t_prcs	process2;
 
+	ft_set_struc(&process1);
+	ft_set_struc(&process2);
 	if (ac != 5)
 		return (0);
 	path = ft_tab_of_paths(envp);
 	if (!path)
 		return (0);
-	cmd1 = ft_handling_command(av[2], path);
-	cmd2 = ft_handling_command(av[3], path);
-	if (!cmd1 || !cmd2)
+	process1.cmd = ft_handling_command(av[2], path);
+	process2.cmd = ft_handling_command(av[3], path);
+	if (!process1.cmd || !process2.cmd)
 		return (0);
 	if (pipe(pi) == -1)
 		return (0);
-	procid = fork();
-	if (procid < 0)
+	process1.id = fork();
+	if (process1.id < 0)
 		return (0);
-	if (procid == 0)
+	if (process1.id == 0)
 	{
-		fd1 = open(av[1], O_RDONLY);
+		process1.fd = open(av[1], O_RDONLY);
 		close(pi[0]);
 		dup2(pi[1], 1);
-		if (fd1 != -1)
+		if (process1.fd != -1)
 		{
-			dup2(fd1, 0);
-			execve(cmd1[0], cmd1, envp);
+			dup2(process1.fd, 0);
+			execve(process1.cmd[0], process1.cmd, envp);
 		}
 		perror("pipex");
 		exit(0);
 	}
 	else
 	{
-		procid2 = fork();
-		if (procid2 < 0)
+		process2.id = fork();
+		if (process2.id < 0)
 			return (0);
-		if (procid2 == 0)
+		if (process2.id == 0)
 		{
-			fd2 = open(av[4], O_CREAT | O_WRONLY | O_TRUNC, 00644);
+			process2.fd = open(av[4], O_CREAT | O_WRONLY | O_TRUNC, 00644);
 			close(pi[1]);
 			dup2(pi[0], 0);
-			if (fd2 != -1)
+			if (process2.fd != -1)
 			{
-				dup2(fd2, 1);
-				execve(cmd2[0], cmd2, envp);
+				dup2(process2.fd, 1);
+				execve(process2.cmd[0], process2.cmd, envp);
 			}
 			perror("pipex");
 			exit(0);
@@ -97,8 +126,8 @@ int
 			while (wait(NULL) != -1);
 		}
 	}
-	ft_free_tab(cmd1);
-	ft_free_tab(cmd2);
+	ft_free_tab(process1.cmd);
+	ft_free_tab(process2.cmd);
 	ft_free_tab(path);
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: sachouam <sachouam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/06 01:05:40 by sachouam          #+#    #+#             */
-/*   Updated: 2021/09/10 00:45:14 by sachouam         ###   ########.fr       */
+/*   Updated: 2021/09/10 03:19:50 by sachouam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,18 @@
 
 # include "libft/libft.h"
 
-void	ft_free_tab(char **tab);
-char	*ft_strjoin_three(char const *s1,
-			char const *s2, char const *s3);
-char	**ft_tab_of_paths(char **envp);
-char	*ft_find_cmd(char **path, char *cmd);
-char	**ft_handling_command(char *arg, char **path);
+typedef struct s_prcs
+{
+	pid_t		id;
+	int			fd;
+	char		**cmd;
+}				t_prcs;
+
+void			ft_free_tab(char **tab);
+char			*ft_strjoin_three(char const *s1,
+					char const *s2, char const *s3);
+char			**ft_tab_of_paths(char **envp);
+char			*ft_find_cmd(char **path, char *cmd);
+char			**ft_handling_command(char *arg, char **path);
 
 #endif
